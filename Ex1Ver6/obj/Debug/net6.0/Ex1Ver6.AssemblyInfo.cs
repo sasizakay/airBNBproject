6f28ee2c9b971d5010ee1010e2bb7002de1252a6
ruppinCgroup58 <cgroup58ruppin@gmail.com>
2024-03-11 21:48:51 +0200

    initial commit

--- Ex1Ver6/obj/Debug/net6.0/Ex1Ver6.AssemblyInfo.cs
+++ Ex1Ver6/obj/Debug/net6.0/Ex1Ver6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex1Ver6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cee42cbf0605486c5807cfddd95a3e17fad9379c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1639abf31f9d026d651de947621e05c326391c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex1Ver6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex1Ver6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
